3ce07214dbba1823440b657092e2f10d7dadbc64
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 05:23:01 -0500

    Added noCache back in

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ const app = express();
 // const timeInSeconds = 90*24*60*60;
 // app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 // app.use(helmet.dnsPrefetchControl());
-// app.use(helmet.noCache());
+app.use(helmet.noCache());
 // app.use(
 //  helmet.contentSecurityPolicy({
 //    directives: {
