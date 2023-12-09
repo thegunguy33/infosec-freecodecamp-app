e182b7382dd030fadb0e4651c52b93ae2f4670b6
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 04:22:38 -0500

    Hide Powered By

--- myApp.js
+++ myApp.js
@@ -1,37 +1,7 @@
 const express = require('express');
+const helmet = require('helmet')
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy());
 
 
 
@@ -55,7 +25,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
