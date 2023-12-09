9834af330a6ece386d6d49fbfbba43d56f8d5900
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 04:46:22 -0500

    Prevent X-Download-Options

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'DENY'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
-
+app.use(helmet.ieNoOpen());
 
 
 module.exports = app;
