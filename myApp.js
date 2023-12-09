c1655919e0252b247c003c77ae26e4369eb25d4c
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-09 04:36:11 -0500

    Added FrameGuard

--- myApp.js
+++ myApp.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const helmet = require('helmet')
 const app = express();
+
+
+
 app.use(helmet.hidePoweredBy());
 
 
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.frameguard({action: 'DENY'}));
 
 
 module.exports = app;
